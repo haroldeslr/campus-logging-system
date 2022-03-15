73d21c953f77c8ae6a5df9a3ee541a303f662000
Harold Blanza <haroldblanza@Harolds-MacBook-Air.local>
2022-03-15 17:15:04 +0800

    Add show/hide password button

--- new-password.php
+++ new-password.php
@@ -23,7 +23,7 @@ if ($email == false) {
         <div class="row">
             <div class="col-md-4 offset-md-4 form">
                 <form action="new-password.php" method="POST" autocomplete="off">
-                    <h2 class="text-center">New Password</h2>
+                    <h2 class="text-center mb-3">New Password</h2>
                     <?php
                     if (isset($_SESSION['info'])) {
                     ?>
@@ -48,10 +48,26 @@ if ($email == false) {
                     }
                     ?>
                     <div class="form-group">
-                        <input class="form-control" type="password" name="password" placeholder="Create new password" required>
+                        <div class="input-group">
+                            <input class="form-control" type="password" id="password" name="password" placeholder="Enter new password" required>
+                            <div class="input-group-append">
+                                <span class="input-group-text" onclick="password_show_hide();">
+                                    <i class="fas fa-eye" id="password-show-eye"></i>
+                                    <i class="fas fa-eye-slash d-none" id="password-hide-eye"></i>
+                                </span>
+                            </div>
+                        </div>
                     </div>
                     <div class="form-group">
-                        <input class="form-control" type="password" name="cpassword" placeholder="Confirm your password" required>
+                        <div class="input-group">
+                            <input class="form-control" type="password" id="cpassword" name="cpassword" placeholder="Confirm new password" required>
+                            <div class="input-group-append">
+                                <span class="input-group-text" onclick="confirm_password_show_hide();">
+                                    <i class="fas fa-eye" id="confirm-password-show-eye"></i>
+                                    <i class="fas fa-eye-slash d-none" id="confirm-password-hide-eye"></i>
+                                </span>
+                            </div>
+                        </div>
                     </div>
                     <div class="form-group">
                         <input class="form-control button" type="submit" name="change-password" value="Change">
@@ -61,6 +77,9 @@ if ($email == false) {
         </div>
     </div>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/solid.min.js" integrity="sha512-+KCv9G3MmyWnFnFrd2+/ccSx5ejo1yED85HZOvNDhtyHu2tuLL8df5BtaLXqsiF68wGLgxxMb4yL5oUyXjqSgw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/js/fontawesome.min.js" integrity="sha512-ywaT8M9b+VnJ+jNG14UgRaKg+gf8yVBisU2ce+YJrlWwZa9BaZAE5GK5Yd7CBcP6UXoAnziRQl40/u/qwVZi4Q==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="js/new-password.js"></script>
 </body>
 
 </html>
