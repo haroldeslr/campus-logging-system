2f84d87cf6becfa0992972041855c9ae918cd0a2
Troy Liam Jarata <troyliamjarata@gmail.com>
2022-03-30 19:18:42 +0800

    added the departments in roles table

--- sql/roles_tbl.sql
+++ sql/roles_tbl.sql
@@ -2,10 +2,10 @@
 -- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 22, 2022 at 12:16 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.3.31
+-- Host: 127.0.0.1
+-- Generation Time: Mar 30, 2022 at 01:17 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -54,7 +54,19 @@ CREATE TABLE `roles_tbl` (
 
 INSERT INTO `roles_tbl` (`id`, `role_name`, `open_dashboard`, `open_logbook`, `edit_log`, `delete_log`, `open_announcement`, `add_announcement`, `edit_announcement`, `delete_announcement`, `open_users`, `add_users`, `edit_users`, `delete_users`, `open_roles_and_permissions`, `add_roles_and_permissions`, `edit_roles_and_permissions`, `delete_roles_and_permissions`) VALUES
 (1, 'Administrator', 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1),
-(2, 'Manager', 1, 1, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1);
+(2, 'GSD', 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(21, 'Tester', 1, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 0, 0),
+(22, 'BASIC ED ', 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(23, 'CAS', 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(24, 'CEA', 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(25, 'MARKETING', 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0),
+(26, 'CHS', 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(27, 'CITE', 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(28, 'CMA', 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(29, 'CCJE', 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(30, 'CELA', 1, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(31, 'Registrar', 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+(32, 'CSDL', 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -74,7 +86,7 @@ ALTER TABLE `roles_tbl`
 -- AUTO_INCREMENT for table `roles_tbl`
 --
 ALTER TABLE `roles_tbl`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
